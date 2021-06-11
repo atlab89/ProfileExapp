a423fb27b35e7e18e568dec8b9c123ebd971e780
Youdono <64512092+tumendalai@users.noreply.github.com>
2021-06-12 01:24:26 +0800

    Profileexapp

---
+++ app/src/main/java/com/example/myprofile/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.myprofile;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_option, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId()==R.id.timetable){
+            startActivity(new Intent(MainActivity.this,MainActivity2.class));
+        }else if(item.getItemId()==R.id.sp){
+            startActivity(new Intent(MainActivity.this,MainActivity3.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/myprofile/MainActivity2.java
@@ -0,0 +1,36 @@
+package com.example.myprofile;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_option, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId()==R.id.home){
+            startActivity(new Intent(MainActivity2.this,MainActivity.class));
+        }else if(item.getItemId()==R.id.sp){
+            startActivity(new Intent(MainActivity2.this,MainActivity3.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
